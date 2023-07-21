f9842b0c2944f962ec5fb1df2c1a40865ec835a2
Arterning <ning.huang.ridson@gmail.com>
2023-07-21 17:34:25 +0800

    feature: Add sql

---
+++ init.sql
@@ -0,0 +1,3 @@
+insert into personal_manage.ranks (id, uid, score, name, color, created_at, updated_at)
+values  ('1', 1, 10, '宁哥', 'orange', '2023-07-19 10:54:05', '2023-07-21 16:10:47'),
+        ('2', 2, 30, '小慧', 'blue', '2023-07-19 10:54:18', '2023-07-21 16:00:50');
