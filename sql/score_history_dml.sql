eb0b32c2a6844967214f728b610bd47c3a56b55c
Arterning <ning.huang.ridson@gmail.com>
2023-08-04 11:32:06 +0800

    add sql

---
+++ sql/score_history_dml.sql
@@ -0,0 +1,15 @@
+insert into personal_manage.score_history (id, partnerId, score, date, create_at, update_at)
+values  (96, '2', 420, '2023-08-02', '2023-08-02 10:03:40', '2023-08-02 10:28:10.807528'),
+        (97, '1', 70, '2023-08-02', '2023-08-02 10:03:57', '2023-08-02 10:03:57.158904');
+
+insert into personal_manage.score_history (partnerId, score, date, create_at, update_at)
+values  ( '2', 420, '2023-08-03', '2023-08-02 10:03:40', '2023-08-02 10:28:10.807528'),
+        ('1', 100, '2023-08-03', '2023-08-02 10:03:57', '2023-08-02 10:03:57.158904');
+
+insert into personal_manage.score_history (partnerId, score, date, create_at, update_at)
+values  ('2', 430, '2023-08-04', '2023-08-02 10:03:40', '2023-08-02 10:28:10.807528'),
+        ('1', 130, '2023-08-04', '2023-08-02 10:03:57', '2023-08-02 10:03:57.158904');
+
+insert into personal_manage.score_history (partnerId, score, date, create_at, update_at)
+values  ('2', 490, '2023-08-05', '2023-08-02 10:03:40', '2023-08-02 10:28:10.807528'),
+        ('1', 160, '2023-08-05', '2023-08-02 10:03:57', '2023-08-02 10:03:57.158904');
