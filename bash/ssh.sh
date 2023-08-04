891751d523102eb1fb2c2a9b7483bec18cc2c1e1
Arterning <ning.huang.ridson@gmail.com>
2023-08-04 17:12:45 +0800

    add bash script

--- bash/ssh.sh
+++ bash/ssh.sh
@@ -2,4 +2,10 @@
 ssh-keygen -t ed25519 -C "your_email@example.com"
 
 # 将密钥传输到您的服务器
-ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.0.2.123
+ssh-copy-id -i ~/.ssh/id_ed25519.pub root@localhost
+
+# 密钥登录
+ssh -i id_ed25519 root@localhost
+
+# 如果已经在config中配置了密钥，那么可以直接这样登录
+ssh root@localhost
