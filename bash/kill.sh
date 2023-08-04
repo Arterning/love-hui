3f05ad47a3442a9edb734647a46f17ce127ace90
Arterning <ning.huang.ridson@gmail.com>
2023-08-04 16:36:25 +0800

    update bash

--- bash/kill.sh
+++ bash/kill.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# 定义要杀死的端口号
-PORT_TO_KILL=8080
+# 检查是否提供了端口号作为脚本参数
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <port>"
+  exit 1
+fi
+
+# 获取脚本的第一个参数，即要杀死的端口号
+PORT_TO_KILL=$1
 
 # 使用lsof查找占用指定端口的进程ID
 PID=$(lsof -t -i :$PORT_TO_KILL)
 
-echo "$PID will be killed"
 
 # 检查PID是否存在，并尝试杀死进程
 if [ -n "$PID" ]; then
