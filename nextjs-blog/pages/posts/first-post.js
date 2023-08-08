b1b20bb5bcfa62dfe0796d00f9c01fd340b78bdb
Arterning <ning.huang.ridson@gmail.com>
2023-08-08 15:14:56 +0800

    Layout component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from "../../components/layout";
 
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -20,6 +21,6 @@ export default function FirstPost() {
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 }
