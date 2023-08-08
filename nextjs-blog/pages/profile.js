06a9058aee0de3c1485a8e902b4b290e64594fd8
Arterning <ning.huang.ridson@gmail.com>
2023-08-08 15:27:11 +0800

    Profile page

---
+++ nextjs-blog/pages/profile.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>Lovely Hui</p>
+                <p>
+                    小慧是个大聪明
+                </p>
+            </section>
+        </Layout>
+    );
+}
